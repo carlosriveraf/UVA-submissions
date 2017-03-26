9614220ecc2e2333125a34de9b7160c832acac5f
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 00:53:49 -0500

    Update and rename 11942 - Lumberjack Sequencing to 11942 - Lumberjack Sequencing.cpp

--- 11942 - Lumberjack Sequencing.cpp
+++ 11942 - Lumberjack Sequencing.cpp
@@ -10,19 +10,19 @@ int main(){
 	while( N-- ) {
 		vector<int> v;
 		
-		for( int i=1 ; i<=10 ; i++ ) {
+		for( int i = 1 ; i <= 10 ; i++ ) {
 			cin>>length;
 			v.push_back( length );
 		}
 		
 		vector<int> v_cmp (v);
-		sort( v_cmp.begin() , v_cmp.end() );
+		sort( v_cmp.begin(), v_cmp.end() );
 		
 		vector<int> v_cmp1 (v_cmp);
-		reverse( v_cmp1.begin() , v_cmp1.end() );
+		reverse( v_cmp1.begin(), v_cmp1.end() );
 		
-		if( v==v_cmp ) { cout<<"Ordered"<<endl; }
-		else if( v==v_cmp1 ) { cout<<"Ordered"<<endl; }
+		if( v == v_cmp ) { cout<<"Ordered"<<endl; }
+		else if( v == v_cmp1 ) { cout<<"Ordered"<<endl; }
 		else{ cout<<"Unordered"<<endl; }
 	}
 					 

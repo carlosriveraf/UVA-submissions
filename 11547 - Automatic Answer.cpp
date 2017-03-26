82d82fa649a303ed24124769a1e20189a26779e3
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 00:47:30 -0500

    Update and rename 11547 - Automatic Answer to 11547 - Automatic Answer.cpp

--- 11547 - Automatic Answer.cpp
+++ 11547 - Automatic Answer.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main(){
 	int t; cin>>t;
-	while(t--){
+	while( t-- ) {
 		int n; cin>>n;
 		int D;
-		n=(((n*567/9)+7492)*235/47)-498;
-		D=abs((n/10)%10);
+		n = ( ( (n * 567 / 9) + 7492 ) * 235 / 47 ) - 498;
+		D = abs( ( n / 10 ) % 10 );
 		cout<<D<<endl;
 	}
 

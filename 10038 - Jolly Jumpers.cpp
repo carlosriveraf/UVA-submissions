b40f30407f3ee39c6279ecfe2055c0320a12fed7
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 00:24:02 -0500

    Update 10038 - Jolly Jumpers.cpp

--- 10038 - Jolly Jumpers.cpp
+++ 10038 - Jolly Jumpers.cpp
@@ -4,20 +4,20 @@ using namespace std;
 int main(){
 	int n;
 	
-	while(cin>>n){
+	while( cin>>n ) {
 		int numbers[n];
 		set<int> jolly;
 		int x;
 		
-		for(int i=0;i<n;i++){cin>>numbers[i];}
+		for( int i = 0 ; i < n ; i++ ) { cin>>numbers[i]; }
 		
-		for(int i=1;i<n;i++){
-			x=abs(numbers[i-1]-numbers[i]);
-			if(x>0&&x<n){jolly.insert(x);}
+		for( int i = 1 ; i < n ; i++ ) {
+			x = abs( numbers[i-1] - numbers[i] );
+			if( x > 0 && x < n ) { jolly.insert(x); }
 		}
 			
-		if(jolly.size()==n-1){cout<<"Jolly"<<endl;}
-		else{cout<<"Not jolly"<<endl;}	
+		if( jolly.size() == n - 1 ) { cout<<"Jolly"<<endl; }
+		else{ cout<<"Not jolly"<<endl; }	
 	}
 		
 	return 0;

746d1940a37e1a14d481b8f1af2e0b49e2a8246a
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 00:54:47 -0500

    Update and rename 12015 - Google is Feeling Lucky to 12015 - Google is Feeling Lucky.cpp

--- 12015 - Google is Feeling Lucky.cpp
+++ 12015 - Google is Feeling Lucky.cpp
@@ -17,14 +17,14 @@ int main(){
 		string url; int priority;
 		vector<string> ans;
 		
-		for( int i=1 ; i<=10 ; i++ ) {
+		for( int i = 1 ; i <= 10 ; i++ ) {
 			cin>>url; cin>>priority;
 			Q.push( Node( url , priority ) );
 		}
 		
 		int v = Q.top().second;
 		
-		for( int i=1 ; i<=10 ; i++ ) {
+		for( int i = 1 ; i <= 10 ; i++ ) {
 			if( Q.top().second == v ) {
 				ans.push_back( Q.top().first );
 				Q.pop();
@@ -32,7 +32,7 @@ int main(){
 		}
 		
 		cout<<"Case #"<<num_case++<<":"<<endl;
-		for( int i=0 ; i<ans.size() ; i++ ) { cout<<ans[i]<<endl; }
+		for( int i = 0 ; i < ans.size() ; i++ ) { cout<<ans[i]<<endl; }
 	}
 				 
     return 0;

277312b04c45f1c3c55c49b369c90fa17d082d5f
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 00:43:05 -0500

    Update and rename 11388 - GCD LCM to 11388 - GCD LCM.cpp

--- 11388 - GCD LCM.cpp
+++ 11388 - GCD LCM.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main(){
 	int T; cin>>T;
-	while(T--){
+	while( T-- ) {
 		int G; cin>>G;
 		int L; cin>>L;
 		
-		if(L%G==0){cout<<G<<" "<<L<<endl;}
-		else{cout<<"-1"<<endl;}	
+		if( L % G == 0 ) { cout<<G<<" "<<L<<endl; }
+		else{ cout<<"-1"<<endl; }	
 	}
 	
 	return 0;

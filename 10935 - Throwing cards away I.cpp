b8a29ebc0da1fc9ac91439c4bba70f5c83edb1ca
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 00:33:47 -0500

    Update and rename 10935 - Throwing cards away I to 10935 - Throwing cards away I.cpp

--- 10935 - Throwing cards away I.cpp
+++ 10935 - Throwing cards away I.cpp
@@ -5,27 +5,27 @@ int main(){
 	int n;
 	int aux;
 	
-	while(cin>>n){
+	while( cin>>n ) {
 		queue<int> deck;
 		
-		if(n!=0){
-			for(int i=1;i<=n;i++){deck.push(i);}
+		if( n != 0 ) {
+			for( int i = 1 ; i <=n ; i++ ) { deck.push(i); }
 			
 			cout<<"Discarded cards:";
 			
-			while(deck.size()>1){
+			while( deck.size() > 1 ) {
 				cout<<" "<<deck.front();
 				deck.pop();
-				aux=deck.front();
+				aux = deck.front();
 				deck.pop();
-				if(!deck.empty()){cout<<",";}
+				if( !deck.empty() ) { cout<<","; }
 				deck.push(aux);				
 			}
 			
 			cout<<"\nRemaining card: "<<deck.front()<<endl;
 			deck.pop();	
 		}
-		else{return 0;}	
+		else{ return 0; }	
 	}
 		
 	return 0;

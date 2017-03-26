60f2eacdbce4e20625557584fd195ff2f180162c
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 01:04:14 -0500

    Update and rename 12372 - Packing for Holiday to 12372 - Packing for Holiday.cpp

--- 12372 - Packing for Holiday.cpp
+++ 12372 - Packing for Holiday.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int main(){
 	int n; cin>>n;
-	int i=1;
-	while(n--){
+	int i = 1;
+	while( n-- ) {
 		int L; cin>>L;
 		int W; cin>>W;
 		int H; cin>>H;
 		
 		cout<<"Case "<<i<<": ";
-		if(L<=20&&W<=20&&H<=20){cout<<"good"<<endl;}
-		else{cout<<"bad"<<endl;}	
+		if( L <= 20 && W <= 20 && H <= 20 ) { cout<<"good"<<endl; }
+		else{ cout<<"bad"<<endl; }	
 		i++;
 	}	
 	

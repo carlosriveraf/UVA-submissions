45646729c73b1b69f3f98fab5d81fb0987a4d42b
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 00:27:36 -0500

    Update and rename 10300 - Ecological Premium to 10300 - Ecological Premium.cpp

--- 10300 - Ecological Premium.cpp
+++ 10300 - Ecological Premium.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main(){
 	int T; cin>>T;
-	while(T--){
+	while( T-- ) {
 		int k; cin>>k;
-		int a,b,c;
-		int s=0;
-		for(int i=1;i<=k;i++){
+		int a, b, c;
+		int s = 0;
+		for( int i = 1 ; i <= k ; i++ ) {
 			cin>>a>>b>>c;
-			s+=(a*c);
+			s += (a*c);
 		}
 		cout<<s<<endl;	
 	}

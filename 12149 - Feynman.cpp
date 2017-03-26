c0faf1a629de48dc6f0300aafab33e3333c09885
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 00:56:17 -0500

    Update and rename 12149 - Feynman to 12149 - Feynman.cpp

---
+++ 12149 - Feynman.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int x;
+	while( cin>>x ) {
+		int suma = 0;
+		if( x == 0 ) return 0;
+		cout<<x * ( x + 1 ) * ( 2 * x + 1 ) / 6<<endl;
+	}
+	
+	return 0;
+}

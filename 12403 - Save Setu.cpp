c84c88c8103f5986354a49ee42e30925adcd5091
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 01:06:00 -0500

    Update and rename 12403 - Save Setu to 12403 - Save Setu.cpp

--- 12403 - Save Setu.cpp
+++ 12403 - Save Setu.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main(){
 	int T; cin>>T;
-	int money=0;
+	int money = 0;
 	
-	while(T--){
+	while( T-- ) {
 		string s; cin>>s;
 		int extra;
 			
-		if( s=="report" ){cout<<money<<endl;}
-		else if( s=="donate"){
+		if( s == "report" ) { cout<<money<<endl; }
+		else if( s == "donate" ) {
 			cin>>extra;
-			money+=extra;
+			money += extra;
 		}	
 	}
 	

2b95cb17af163d6414b29e51ac2d61d9435132b2
pambisito <carlos_2017_1@hotmail.com>
2017-03-26 00:38:25 -0500

    Update and rename 11172 - Relational Operator to 11172 - Relational Operator.cpp

--- 11172 - Relational Operator.cpp
+++ 11172 - Relational Operator.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main(){
 	int T; cin>>T;
-	while(T--){
+	while( T-- ) {
 		int i; cin>>i;
 		int j; cin>>j;
 		
-		if(i>j){cout<<">"<<endl;}
-		else if(i<j){cout<<"<"<<endl;}
-		else{cout<<"="<<endl;}	
+		if( i > j ) { cout<<">"<<endl; }
+		else if( i < j ) { cout<<"<"<<endl; }
+		else{ cout<<"="<<endl; }	
 	}
 	return 0;
 }

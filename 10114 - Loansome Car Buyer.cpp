adddc3cba0f9f274e3d0623e513d5d6862df5c91
pambisito <carlos_2017_1@hotmail.com>
2017-07-31 19:55:12 -0500

    Update 10114 - Loansome Car Buyer.cpp

--- 10114 - Loansome Car Buyer.cpp
+++ 10114 - Loansome Car Buyer.cpp
@@ -15,24 +15,24 @@ int main(){
     	float mp = al / dl;
     	vector<double> md( dl + 1, 0 );
     	int month;
-		double depreciation; 
-		while( nr-- ) {
+	double depreciation; 
+	while( nr-- ) {
     		cin >> month >> depreciation;
     		md[month] = depreciation;
-		}
-		for( int i = 1 ; i < dl + 1 ; i++ ) {
-			if( md[i] == 0) md[i] = md[i-1];
-		}
-		float iv = al + dp;
-		month = 0;   	
+	}
+	for( int i = 1 ; i < dl + 1 ; i++ ) {
+		if( md[i] == 0) md[i] = md[i-1];
+	}
+	float iv = al + dp;
+	month = 0;   	
     	while(1) {
     		iv *= ( 1 - md[month] ) ;
     		if( iv > al && month == 1 ) {
     			cout << month << " month" << endl; break;
-			}
+		}
     		else if( iv > al && month != 1 ) {
-				cout << month << " months" << endl; break;	
-			}
+			cout << month << " months" << endl; break;	
+		}
     		al -= mp;
     		month++;
 		}    	

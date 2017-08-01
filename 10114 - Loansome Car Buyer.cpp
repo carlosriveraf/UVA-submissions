f655d5aa57c3d6f0497cbf46dcc298bd8d3ba1b7
pambisito <carlos_2017_1@hotmail.com>
2017-07-31 19:52:59 -0500

    Create 10114 - Loansome Car Buyer.cpp

---
+++ 10114 - Loansome Car Buyer.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+// duration loan   ---> dl
+// down payment    ---> dp
+// amount loan     ---> al
+// number records  ---> nr
+// monthly payment ---> mp
+// month depreciation ---> md
+// initial value ---> iv
+int main(){
+    int dl, nr;
+    double dp, al;
+    while( cin >> dl >> dp >> al >> nr ) {
+    	if( dl < 0 ) return 0;
+    	float mp = al / dl;
+    	vector<double> md( dl + 1, 0 );
+    	int month;
+		double depreciation; 
+		while( nr-- ) {
+    		cin >> month >> depreciation;
+    		md[month] = depreciation;
+		}
+		for( int i = 1 ; i < dl + 1 ; i++ ) {
+			if( md[i] == 0) md[i] = md[i-1];
+		}
+		float iv = al + dp;
+		month = 0;   	
+    	while(1) {
+    		iv *= ( 1 - md[month] ) ;
+    		if( iv > al && month == 1 ) {
+    			cout << month << " month" << endl; break;
+			}
+    		else if( iv > al && month != 1 ) {
+				cout << month << " months" << endl; break;	
+			}
+    		al -= mp;
+    		month++;
+		}    	
+	}
+
+    return 0;
+}
